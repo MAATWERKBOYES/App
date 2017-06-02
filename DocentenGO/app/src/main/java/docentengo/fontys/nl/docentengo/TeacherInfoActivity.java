b811871383ad510669d1a0d096f96e4b7a4d9a7b
Dekkers Los,Arno A.J.P <a.dekkerslos@student.fontys.nl>
2017-06-02 14:21:40 +0200

    added functionality return button on the Teacher info screen

--- DocentenGO/app/src/main/java/docentengo/fontys/nl/docentengo/TeacherInfoActivity.java
+++ DocentenGO/app/src/main/java/docentengo/fontys/nl/docentengo/TeacherInfoActivity.java
@@ -1,7 +1,12 @@
 package docentengo.fontys.nl.docentengo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import Business.User;
 
 public class TeacherInfoActivity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class TeacherInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_info);
+        createButtonReturnDexEvent();
+    }
+
+    private void createButtonReturnDexEvent(){
+        Button returnButton = (Button)findViewById(R.id.btnReturnDex);
+        returnButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),TeacherDexActivity.class);
+                intent.putExtra("CurrentUser", (User)getIntent().getExtras().getSerializable("CurrentUser"));
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
