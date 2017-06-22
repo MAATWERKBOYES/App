5f6378dae13840ab1f3d7c41f1a4d626a26617a1
Lorenzo Van der Weerden <lorenzovdweerden@hotmail.com>
2017-06-22 12:37:39 +0200

    Lorenoz is een God!

--- DocentenGO/app/src/main/java/docentengo/fontys/nl/docentengo/RankingActivity.java
+++ DocentenGO/app/src/main/java/docentengo/fontys/nl/docentengo/RankingActivity.java
@@ -2,22 +2,19 @@ package docentengo.fontys.nl.docentengo;
 
 import android.content.Intent;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.Collections;
-
 import java.util.List;
 
-import Business.Person;
 import Business.RankingEntry;
-import api.ApiController;
 import Business.User;
+import api.ApiController;
 
 public class RankingActivity extends AppCompatActivity {
     private User signedUser;
